0839600718192bd89d870d55f4d4aaee2c9398a6
Danial Toor <dtoor14@gmail.com>
2023-08-13 21:50:21 -0500

    added properties to the class

--- API/Entities/Product.cs
+++ API/Entities/Product.cs
@@ -2,16 +2,20 @@ namespace API.Entities
 {
     public class Product
     {
-        public int Id {
-            get; 
-            set;
-        }
+        public int Id {get; set;}
         public string Name {get; set;}
         public long Price {get; set;}
 
         public string Type {get; set;}
 
         public string PictureURL {get; set;}
+
+        public int QuantityInStock { get; set; }
+
+        public string Brand { get; set;}
+
+        public int MyProperty { get; set; }
+        
 
     }
 }
